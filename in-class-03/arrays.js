52ababc278518e71b761950f2faeef2829d998fb
Bobby <rhieger@nyc.rr.com>
2017-05-17 19:33:35 -0400

    Completed In Class Arrays Exercise

---
+++ in-class-03/arrays.js
@@ -0,0 +1,18 @@
+var food = ['hotdog', 'pizza', 'taco', 'steak', 'chicken'];
+
+// Printing out select array elements:
+
+// console.log(food[0]);
+// console.log(food[1]);
+
+// var lastIndex = food.length -1;
+
+// var lastElement = food[lastIndex];
+
+// console.log(lastElement);
+
+for (var i = 0; i < food.length; i++) {
+
+	console.log(food[i]);
+
+}	// end for
