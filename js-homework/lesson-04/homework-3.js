b0216f67f7cc74178035382089942a9ba559e0ab
Bobby <rhieger@nyc.rr.com>
2017-05-25 12:29:40 -0400

    Minor Refinement to Code Comments--Homework 3

--- js-homework/lesson-04/homework-3.js
+++ js-homework/lesson-04/homework-3.js
@@ -4,32 +4,34 @@
 //
 // Due by May 25, 11:59 PM
 
+// OBJECTIVE:
+
 /******************************************************************************
- *																			  *
- * Given an array with these elements in it:   								  *
- *																			  *
- * var numberList = [ 1, 5, 2, 20, 7, 90, 3 ];								  *
- *																			  *
- * Write a function that returns the largest element in an array.			  *
- *																			  *
+ *                                                                            *
+ * Given an array with these elements in it:                                  *
+ *                                                                            *
+ * var numberList = [ 1, 5, 2, 20, 7, 90, 3 ];                                *
+ *                                                                            * 
+ * Write a function that returns the largest element in an array.             *
+ *                                                                            *
  * Write a function that checks whether an element occurs in an array. Make   *
- * the function return true or false. For example: 							  *
- *																			  *
- * function inArray(array, element) { ... }									  *
- *																			  *
- * if (inArray(numberList, 3)) { console.log("It exists!") }				  *
- *																			  *
- * Write a function that returns the elements on odd positions in an array.	  *
- * For the list given above, it would print 5, 20, 90						  *
- *																			  *
- * Write a function that computes the running total of an array. (IE: What	  *
- * is the sum of all of the values in an array)								  *
- *																			  *
+ * the function return true or false. For example:                            *
+ *                                                                            *
+ * function inArray(array, element) { ... }                                   *
+ *                                                                            *
+ * if (inArray(numberList, 3)) { console.log("It exists!") }                  *
+ *                                                                            *
+ * Write a function that returns the elements on odd positions in an array.   *
+ * For the list given above, it would print 5, 20, 90                         *
+ *                                                                            *
+ * Write a function that computes the running total of an array. (IE: What    *
+ * is the sum of all of the values in an array)                               *
+ *                                                                            *
  * Write a function that tests whether a string is a palindrome. For example  *
- * "racecar" would return true, but "cat" would not.						  *
- *																			  *
- * You can do all of these in 1 javascript file.							  *
- *																			  *
+ * "racecar" would return true, but "cat" would not.                          *
+ *                                                                            *
+ * You can do all of these in 1 javascript file.                              *
+ *                                                                            *
  ******************************************************************************
 */
 
