1f2e8fdb81c3128406999219a20f2b73427e96b4
Bobby <rhieger@nyc.rr.com>
2017-05-31 19:00:16 -0400

    Modified .gitignore

---
+++ in-class-06/fizzbuzz.js
@@ -0,0 +1,15 @@
+// fizzbuzz.js
+
+/*
+    Write a program that prints the numbers from 1 to 100. But for multiples of three print
+    “Fizz” instead of the number and for the multiples of ﬁve print “Buzz”. For numbers which
+    are multiples of both three and ﬁve print “FizzBuzz"
+*/
+
+for (var i = 1; var <= 100; i++)	{
+
+	// FINISH THIS AS PART OF HOMEWORK.
+
+	console.log()
+
+}	// end for
