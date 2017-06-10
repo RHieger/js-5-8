dd0448192a24bd9538365677547b24d6930b4f8f
Robert <rhieger@nyc.rr.com>
2017-06-09 21:09:55 -0400

    Class 07 Exercises Complete

--- in-class-07/jquery_exercise/main.js
+++ in-class-07/jquery_exercise/main.js
@@ -3,9 +3,11 @@
 You'll add the ability to complete tasks in the list. Your program must complete the following rules:
 
 - Through jQuery add a "complete task" link at the end of each to-do item.
-- When clicked, the link will cross out the current task (hint: add a class to the list that sets the text-decoration to strikeout).
+- When clicked, the link will cross out the current task (hint: add a class to
+  the list that sets the text-decoration to strikeout).
 - Each new task added by the user needs to also have the "complete task" link akt the end.
-- Bonus: when user completes task, remove the task from the current list and add it to a completed list below. Do not show this list unless the there are completed items.
+- Bonus: when user completes task, remove the task from the current list and add it to a completed
+  list below. Do not show this list unless the there are completed items.
 - Bonus 2: add the ability to restore items from the completed list to the first list.
 
 */
@@ -21,9 +23,12 @@ $(function() {
   // button.onclick = function(event) {
   $button.on('click', function(event) {
     event.preventDefault();
+
     MyApp.addToList($thingList);
   });
 });
+
+$('<a href="#">Task Complete</a>').appendTo($thingList 'li');
 
 var MyApp = {};
 
