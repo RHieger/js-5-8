1af3954814a94c5bbb655a88739c12f722f4c7fe
Robert <rhieger@nyc.rr.com>
2017-06-17 17:30:45 -0400

    Lesson 10 Homework—Added Kitten Image

---
+++ js-homework/lesson-10/fun-project/js/risk-it.js
@@ -0,0 +1,21 @@
+// risk-it.js
+//
+// Lesson 10 Homework
+//
+// Robert Hieger
+
+// Wait for DOM to load.
+
+$(document).ready(function()    {
+
+    $('#risk-it-button').on('click', function() {
+
+        // setTimeout.(function()  {
+
+            
+
+        // });
+
+    }); // $('#risk-it-button').on('click')
+    
+}); // end $(document).ready()
