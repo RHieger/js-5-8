d5784e7f8b6cf89d514bc8444270fbeed4579856
Robert <rhieger@nyc.rr.com>
2017-06-17 00:38:43 -0400

    In Class Fun Project Complete

--- in-class-10/in-class-fun/js/video.js
+++ in-class-10/in-class-fun/js/video.js
@@ -1,1 +1,32 @@
 // video.js
+
+$(document).ready(function()   {
+
+  var videoIds = ['APJsXF2TtFg', 'HxM46vRJMZs'];
+
+  $('#random-video-btn').on('click', function(){
+
+    $('#random-video').empty();
+
+    var randIndex = Math.ceil(Math.random() * 100) % videoIds.length;
+
+    console.log(videoIds[randIndex]);
+
+    var element = $('<iframe width="100%" height="315" src="https://www.youtube.com/embed/' +
+    	videoIds[randIndex] + '" frameborder="0" allowfullscreen></iframe>');
+
+    $('#random-video').append(element);
+
+    $('#add-video').on('click', function(){
+
+      var currentID = $('#random-video-id').val();
+
+      videoIds.push(currentID);
+
+      $('#random-video-id').val("");
+
+    });	// end $('#add-video').on('click')
+
+  });	// end $('#random-video-btn').on('click')
+
+});	// end $(document).ready()
